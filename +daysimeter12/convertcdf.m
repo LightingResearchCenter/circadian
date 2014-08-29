5c8f31f7100352d4d28a7359769e5b8dc41ff1c5
Geoffrey Jones <jonesg5@rpi.edu>
2014-08-29 16:39:34 -0400

    Renaming and reorganizing

--- +daysimeter12/convertcdf.m
+++ +daysimeter12/convertcdf.m
@@ -9,6 +9,11 @@ absTime = absolutetime(Data.Variables.time(:),'cdfepoch',false,...
 relTime = relativetime(Data.Variables.time(:),'cdfepoch',false,...
     Data.Variables.timeOffset,'seconds');
 
+red = Data.Variables.red;
+green = Data.Variables.green;
+blue = Data.Variables.blue;
+chromaticity = daysimeter12.rgb2chrom(red,green,blue);
+
 light = []; % placeholder
 
 activity = Data.Variables.activity(:);
