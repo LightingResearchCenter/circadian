c7a761cb5367ea66d9576f0efae16ad39fa1d44e
Geoffrey Jones <jonesg5@rpi.edu>
2014-09-02 15:34:23 -0400

    Added comments to chrom2cct

--- +lighttools/chrom2cct.m
+++ +lighttools/chrom2cct.m
@@ -1,14 +1,16 @@
 function cct = chrom2cct(x,y)
-%CHROM2CCT
-%	CCT (Correlated Color Temperature)
+%CHROM2CCT Calculate CCT from CIE 1931 chromaticity coordinates (x,y)
 %	Calculates the correlated color temperature (CCT) from CIE x, y values.
 %	Uses method by Allen Robertson as references in W&S (1982).
 %	Needs file 'isoTempLines.mat' for isotemperature lines from W&S (1982) 
 %   or similarly calculated.
 %   
-%	Function arguments are:
-%       x - CIE 1931 x value
-%       y - CIE 1931 y value
+%	Inputs:
+%       x = CIE 1931 x value
+%       y = CIE 1931 y value
+%   
+%   Output:
+%       cct = correlated color temperature in degrees Kelvin
 %   
 %   EXAMPLE:
 %       cct = lighttools.chrom2cct(x,y)
@@ -28,13 +30,13 @@ for iPoint = 1:nPoint
     % Find adjacent lines to (us,vs)
 	d = ((vs(iPoint)-vt) - tt.*(us(iPoint)-ut))./sqrt(1+tt.*tt);
     
-    d1 = d(1:end-1);
-    d2 = d(2:end);
+    d1 = d(1:end-1); % Distance from point to iso lines 1 to end-1
+    d2 = d(2:end);   % Distance from point to iso lines 2 to end
     
-    ratioD = d1./d2;
+    ratioD = d1./d2; % Ratio of distances to iso lines
     
-    lessThan0 = ratioD < 0;
-    equalTo0 = d == 0;
+    lessThan0 = ratioD < 0; % Negative ratios (between these lines)
+    equalTo0 = d == 0;      % Zero distance (directly on a line)
     
     % Directly on a line
     if any(equalTo0)
@@ -50,6 +52,7 @@ for iPoint = 1:nPoint
         cct(iPoint) = 1/(1/Tj+D1/(D1-D2)*(1/Tj1-1/Tj));
     end
     % Otherwise not able to calculate CCT, u,v coordinates outside range.
+    % No value will be written to cct(iPoint) and will remain NaN
 end
 
 end
