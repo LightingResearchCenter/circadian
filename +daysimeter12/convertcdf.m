e1f3162f65b1536bbd2eb84986129e31ac6a2a58
Geoffrey Jones <jonesg5@rpi.edu>
2014-09-04 11:35:08 -0400

    Created eventmasks class

--- +daysimeter12/convertcdf.m
+++ +daysimeter12/convertcdf.m
@@ -9,19 +9,41 @@ absTime = absolutetime(Data.Variables.time(:),'cdfepoch',false,...
 relTime = relativetime(Data.Variables.time(:),'cdfepoch',false,...
     Data.Variables.timeOffset,'seconds');
 
-red = Data.Variables.red;
-green = Data.Variables.green;
-blue = Data.Variables.blue;
+red = Data.Variables.red(:);
+green = Data.Variables.green(:);
+blue = Data.Variables.blue(:);
 
-illuminance = Data.Variables.illuminance;
-cla = Data.Variables.CLA;
+illuminance = Data.Variables.illuminance(:);
+cla = Data.Variables.CLA(:);
 
 chromaticity = daysimeter12.rgb2chrom(red,green,blue);
 
-light = lightmetrics('illuminance',illuminance,'cla',cla,'chromaticity',chromaticity); % placeholder
+light = lightmetrics('illuminance',illuminance,'cla',cla,...
+    'chromaticity',chromaticity);
 
 activity = Data.Variables.activity(:);
 
-masks = []; % placeholder
+
+% Check what logical masks are available.
+if isfield(Data.Variables,'logicalArray')
+    observation = logical(Data.Variables.logicalArray(:));
+else
+    observation = logical([]);
+end
+if isfield(Data.Variables,'complianceArray')
+    compliance = logical(Data.Variables.complianceArray(:));
+else
+    compliance = logical([]);
+end
+if isfield(Data.Variables,'bedArray')
+    bed = logical(Data.Variables.bedArray(:));
+else
+    bed = logical([]);
+end
+
+% Create eventmasks object called masks.
+masks = eventmasks('compliance',compliance,'observation',observation,...
+    'bed',bed);
+
 end
 
