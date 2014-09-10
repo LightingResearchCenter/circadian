46c68dc40933ef1f243810df6cf5e5ad2ba2eff1
Geoffrey Jones <jonesg5@rpi.edu>
2014-09-10 17:59:10 -0400

    Reorganized Contents

--- Contents.m
+++ Contents.m
@@ -1,16 +1,15 @@
-% Circadian Analysis Library
+% Circadian Analysis Toolbox
 % Version 0.1 10-Sep-2014
 %
-% Files
+% Classes
 %   absolutetime       - Stores absolute local and UTC time in several formats.
-%   chromcoord         - Summary of this class goes here
-%   enrightperiodogram - Calculates the Enright periodogram on column vector
-%   eventmasks         - Logical arrays to mark certain events
-%   isivcalc           - 
-%   lightmetrics       - Storage and conversion for multiple light metrics.
-%   phasor             - Compares two time dependent signals
 %   relativetime       - Stores time relative to an absolute start time.
 %   samplingrate       - Summary of this class goes here
-%   test               - 
-%   testread           - 
-%   utcoffset          - Summary of this class goes here
+%   lightmetrics       - Storage and conversion for multiple light metrics.
+%   chromcoord         - Summary of this class goes here
+%   eventmasks         - Logical arrays to mark certain events
+%
+% Functions
+%   enrightperiodogram - Calculates the Enright periodogram on column vector
+%   isivcalc           - 
+%   phasor             - Compares two time dependent signals
