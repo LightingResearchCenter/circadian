4b89a150acbf99d7ba32c835e38563774f750764
Geoffrey Jones <jonesg5@rpi.edu>
2014-09-10 10:42:37 -0400

    Created top-level Contents.m

---
+++ Contents.m
@@ -0,0 +1,15 @@
+% CIRCADIANANALYSISLIBRARY
+%
+% Files
+%   absolutetime       - Stores absolute local and UTC time in several formats
+%   chromcoord         - Summary of this class goes here
+%   enrightperiodogram - ENRIGHTPERGRAM Calculates the Enright periodogram on column vector
+%   eventmasks         - Logical arrays to mark certain events
+%   isivcalc           - 
+%   lightmetrics       - Summary of this class goes here
+%   phasor             - Compares two time dependent signals
+%   relativetime       - Summary of this class goes here
+%   samplingrate       - Summary of this class goes here
+%   test               - 
+%   testread           - 
+%   utcoffset          - Summary of this class goes here
