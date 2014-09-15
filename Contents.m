23193dd0af5d36e0380bbee5e342f299ab400103
Geoffrey Jones <jonesg5@rpi.edu>
2014-09-15 15:31:08 -0400

    Updated Contents.m

--- Contents.m
+++ Contents.m
@@ -10,18 +10,13 @@
 % Classes
 %   absolutetime       - Stores absolute local and UTC time in several formats.
 %   relativetime       - Stores time relative to an absolute start time.
-%   samplingrate       - Summary of this class goes here
-%   utcoffset          - Summary of this class goes here
+%   samplingrate       - Data collection sampling rate in several units.
+%   utcoffset          - Offset of local time from UTC in several units.
 %   lightmetrics       - Storage and conversion for multiple light metrics.
 %   chromcoord         - Stores chromaticity coordinates in CIE 1931, 1960, and 1976
-%   eventmasks         - Logical arrays to mark certain events
-%
-% Functions
-%   enrightperiodogram - Calculates the Enright periodogram on column vector
-%   isivcalc           - 
-%   phasor             - Compares two time dependent signals
+%   eventmasks         - Logical arrays to mark certain events.
 %
 % Packages
-% See also daysimeter12, lightcalc, phasor, shared
+% See also daysimeter12, isiv, lightcalc, periodograms, phasor, shared
 
 % Copyright 2014-2014 Rensselaer Polytechnic Institute
