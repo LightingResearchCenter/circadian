237a0e97cd4812f8754ebfee10cb724e56a050bf
Geoffrey Jones <jonesg5@rpi.edu>
2014-11-07 10:31:17 -0500

    Created phasor.prep

--- +phasor/prep.m
+++ +phasor/prep.m
@@ -15,14 +15,14 @@ activity(~masks.observation) = [];
 if ~isempty(masks.compliance)
     masks.compliance(~masks.observation) = [];
 end
-masks.observation(~masks.observation) = [];
-
 
 if ~isempty(masks.bed)
     masks.bed(~masks.observation) = [];
     cs(masks.bed) = 0;
     activity(masks.bed) = 0;
 end
+
+masks.observation(~masks.observation) = [];
 
 if ~isempty(masks.compliance) && ~isempty(masks.bed)
     masks.compliance = adjustcrop(time,masks.compliance,masks.bed);
@@ -34,26 +34,10 @@ if ~isempty(masks.compliance)
     activity(~masks.compliance) = [];
 end
 
-% Apply gaussian filter to data
-filterWindow = ceil(300/epoch.seconds); % approximate number of samples in 5 minutes
-cs = gaussian(cs,filterWindow);
-activity = gaussian(activity,filterWindow);
-
 Phasor = struct;
 [Phasor.vector,Phasor.magnitude,Phasor.angle,...
     Phasor.magnitudeHarmonics,Phasor.firstHarmonic] = ...
     phasor.phasor(time,epoch,cs,activity);
 
 end
-
-function filtered = gaussian(data, window)
-
-%window is number of points on each side of current point that you want to
-%consider
-window = window*2;
-filtered = filtfilt(gausswin(window)/sum(gausswin(window)), 1, data);
-
-end
-
-
 
