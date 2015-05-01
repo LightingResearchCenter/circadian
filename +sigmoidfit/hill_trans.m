9d4e00864113d8e5989e23695ce3c2f42bf1d1f2
Geoffrey Jones <jonesg5@rpi.edu>
2015-05-01 15:58:18 -0400

    Changed variable order

--- +sigmoidfit/hill_trans.m
+++ +sigmoidfit/hill_trans.m
@@ -1,8 +1,8 @@
-function r = hill_trans(t,phi,k,gamma,m,minR,amp)
+function r = hill_trans(t,minR,amp,phi,m,gamma)
 %HILL_TRANS Computes sigmoidally transformed Hill function
 %   Detailed explanation goes here
 
-c = sigmoidfit.cos_trans(t,phi,k);
+c = sigmoidfit.cos_trans(t,phi);
 x = c + 1;
 h = sigmoidfit.hill(x,gamma,m);
 r = minR + amp.*h;
