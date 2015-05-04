b1f42fe94ada78bfb4d22de8b27a9c8ba511023d
Geoffrey Jones <jonesg5@rpi.edu>
2015-05-04 14:53:14 -0400

    Created sigmoidfit.cos_fit

---
+++ +sigmoidfit/cos_fit.m
@@ -0,0 +1,18 @@
+function cm = cos_fit(X,y)
+%COS_FIT Summary of this function goes here
+%   Detailed explanation goes here
+
+% Fit cosine using linear least squares
+k = (2*pi)/24; % ratio of radians per period, period in units of t
+chi = [sin(X.*k), cos(X.*k)];
+mes = mean(y);
+y2 = y - mes;
+b = chi\y2;
+
+amp = norm(b);
+phi = atan2(b(1),b(2))/k;
+
+fitted = mes + amp.*cos((X - phi).*k);
+
+cm = struct('mes',mes,'amp',amp,'phi',phi,'Fitted',fitted);
+end
