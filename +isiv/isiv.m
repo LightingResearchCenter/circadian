84b932fa491f49b2ddcfb51eb60ce39f8ad5953b
Geoffrey Jones <jonesg5@rpi.edu>
2015-05-18 12:24:45 -0400

    Made isiv consistent with ISm and IVm

--- +isiv/isiv.m
+++ +isiv/isiv.m
@@ -1,4 +1,4 @@
-function [interdailyStability,intradailyVariability] = isiv(dataArray,epoch)
+function [IS,IV] = isiv(dataArray,epoch)
 % ISIV Calculates interdaily stability (IS) and intradaily variability (IV)
 %	Returns the interdaily stability and the intradaily variability 
 %   statistic for time series dataArray.
@@ -7,15 +7,14 @@ function [interdailyStability,intradailyVariability] = isiv(dataArray,epoch)
 %	Converts data to hourly increments before calculating metric.
 %
 % EXAMPLE:
-%   [interdailyStability,intradailyVariability] = ...
-%   isiv.isiv(dataArray,epoch);
+%   [IS,IV] = isiv.isiv(dataArray,epoch);
 %
 % See also SAMPLINGRATE.
 
 import shared.nonanmean periodograms.enright;
 
-n = numel(dataArray);
-if (n < 24 || n*epoch.hours < 24)
+n1 = numel(dataArray);
+if (n1 < 24 || n1*epoch.hours < 24)
     error('Cannot compute statistic because time series is less than 24 hours');
 end
 
@@ -27,24 +26,28 @@ if (rem(1/epoch.hours,1) > eps)
     warning('epoch does not divide into an hour without a remainder');
 end
 
-% Convert to hourly data increments
-nHours = floor(n*epoch.hours); % Number of whole hours of data
-hourlyDataArray = zeros(nHours,1); % Preallocate Ahourly
-for i1 = 1:nHours % 1 to the number of hours of data
-    idx = floor((i1-1)/epoch.hours+1):floor(i1/epoch.hours);
-    hourlyDataArray(i1) = nonanmean(dataArray(idx));
-end
+% Make sure dataArray spans whole days, remove excess from end
+nDay = floor(n1*epoch.minutes/1440);
+n2 = floor(nDay*1440/epoch.minutes);
+dataArray = dataArray(1:n2);
 
-maxPeriod = 24; % maximum period in hours
+% Resample data to hourly increments
+X_i = isiv.resample(dataArray,epoch,60);
+% Number of samples
+n = numel(X_i);
+% Number of samples per day
+p = 24;
+% Average of samples at each time of day
+Xmat = reshape(X_i,[p,nDay]);
+Xbar_h = mean(Xmat,2);
+% Average of all samples
+Xbar = mean(X_i);
 
-period = enright(hourlyDataArray,maxPeriod);
+% Calculate Interdaily Stability
+IS = (n*sum((Xbar_h - Xbar).^2))/(p*sum((X_i - Xbar).^2));
+% Calculate Intradail Variability
+IV = (n*sum(diff(X_i).^2))/((n-1)*sum((X_i-Xbar).^2));
 
-interdailyStability = period^2/var(hourlyDataArray,1);
-
-hourlyMean = mean(hourlyDataArray);
-
-intradailyVariability =  (sum(diff(hourlyDataArray).^2)/(nHours-1))...
-                        /(sum((hourlyDataArray-hourlyMean).^2)/nHours);
 
 
 end
