221ffdaefbcdad83954520d21bc0a5c284eab71e
Geoffrey Jones <jonesg5@rpi.edu>
2015-10-25 18:41:09 -0400

    Added chronotype kit

---
+++ +chronotype/Chronotype.m
@@ -0,0 +1,43 @@
+function MSFsc = Chronotype(tDay,AI,SPrep,GU)
+% CHRONOTYPE Calculates the sleep debt corrected free day mid-sleep time
+%   t = t in matlab DATENUM format
+%   AI = activity index
+%   SPrep = local time of preparing to sleep
+%   GU = local time of getting out of bed
+%   MSFsc = mid-sleep on free days corrected for accumulated sleep dept
+
+%% Eliminate SPrep and GU that are outside of the analysis range
+idxElim = SPrep < tDay(1) | SPrep > tDay(end) | ...
+             GU < tDay(1) | GU > tDay(end);
+SPrep(idxElim) = [];
+GU(idxElim) = [];
+
+%% Initialize variables
+TD = length(SPrep); % total number of days to be analyzed
+SO = zeros(TD,1); % sleep onset in seconds
+SE = zeros(TD,1); % sleep end in seconds
+SD = zeros(TD,1); % sleep duration in seconds
+MS = zeros(TD,1); % basic mid-sleep in seconds
+idxW = false(TD,1); % workday logical
+
+%% Calculate basic Roennenberg variables for each day
+for i1 = 1:TD
+    [SO(i1),SE(i1),SD(i1),MS(i1),idxW(i1)] = ...
+        RoennenbergMethod(tDay,AI,SPrep(i1),GU(i1));
+end
+
+%% Calculate workday variables
+WD = sum(idxW); % number of workdays
+SDw = mean(SD(idxW)); % sleep duration on workdays in seconds
+
+%% Calculate free day variables
+FD = sum(~idxW); % number of free days
+SDf = mean(SD(~idxW)); % sleep duration on free days in seconds
+SDweek = (SDw*WD + SDf*FD)/TD; % average sleep duration for the week in seconds
+MSF = mean(MS(~idxW)); % mid-sleep on free days in seconds
+MSFsc = MSF - (SDf - SDweek)/2; % corrected mid-sleep on free days in seconds
+
+%% Convert MSFsc to days
+MSFsc = mod(MSFsc/86400,1); % corrected mid-sleep on free days in days
+
+end
---
+++ +chronotype/RoennenbergMethod.m
@@ -0,0 +1,82 @@
+function [SO,SE,SD,MS,W] = RoennenbergMethod(t,AI,SPrep,GU)
+%ROENNENBERGMETHOD Calculates sleep parameters using Roennenberg method
+%   Combines Actiware sleep algorithim with Roennenberg sleep parameters
+%   t = t in matlab DATENUM format
+%   AI = activity index
+%   SPrep = local time of preparing to sleep
+%   GU = local time of getting out of bed
+
+%% Trim data to within the analysis plus a buffer
+buffer = 5/(60*24); % 5 minute buffer
+idx = t >= (SPrep - buffer) & t <= (GU + buffer);
+t = t(idx);
+if numel(t) == 0
+    error('sleep prep and get up time out of range');
+end
+AI = AI(idx);
+
+%% Determine if the analysis ends on a workday
+dayNumber = weekday(t(end));
+if dayNumber == 1 || dayNumber == 7
+    W = false;
+else
+    W = true;
+end
+
+%% Attempt to apply a gaussian filter to Activity
+try
+    AI = gaussian(AI,4);
+catch % err
+%     warning(err.message);
+end
+
+%% Convert time to seconds from start of first day
+tSec = (t - floor(t(1)))*86400;
+
+%% Calculate the sampling epoch to the nearest second
+tempSec1 = tSec(2:end);
+tempSec2 = circshift(tSec,1);
+tempSec2(1) = [];
+epoch = round(mean(abs(tempSec1-tempSec2)));
+
+%% Find the sleep state (SS)
+SS = FindSleepState(AI,'auto',3);
+
+%% Find sleep onset (SO) and sleep end (SE)
+% Find sleep state in a 10 minute window
+n10 = ceil(600/epoch); % Number of points in a 10 minute interval
+n5 = floor((n10)/2);
+AS = ~SS; % Active state (AS)
+AS10 = AS; % Initialize 10 minute active state (AS10)
+for i1 = -n5:n5
+    AS10 = AS10 + circshift(AS,i1);
+end
+SS10 = AS10 <= 1; % Create 10 minute sleep state (SS10)
+tSec2 = tSec;
+
+% Remove first and last 10 minutes
+last = numel(tSec2);
+tSec2((last-n5):last) = [];
+SS10((last-n5):last) = [];
+tSec2(1:n5) = [];
+SS10(1:n5) = [];
+
+% Find sleep onset (SO)
+idxO = find(SS10,true,'first');
+SO = tSec2(idxO);
+
+% Find sleep end (SE)
+idxE = find(SS10,true,'last');
+SE = tSec2(idxE);
+
+%% Calculate sleep duration (SD) in seconds
+SD = abs(SE - SO);
+
+%% Calculate basic mid-sleep (MS) in seconds
+MS = mod(SO + SD/2,86400);
+% if mid-sleep occurs between midnight and noon add 24 hours
+if MS < 43200
+    MS = MS + 86400;
+end
+
+end
