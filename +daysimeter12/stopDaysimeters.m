17fd587cc5b8d2669d6e28c00f4b857a1d8cdcc4
plummt <plummt@rpi.edu>
2016-04-02 22:36:49 -0400

    Added correct stopDaysimeters
    githud didnt grab the file properly. this should correct for it

--- +daysimeter12/stopDaysimeters.m
+++ +daysimeter12/stopDaysimeters.m
@@ -1,7 +1,25 @@
-function [ output_args ] = stopDaysimeters( input_args )
+function [ output_args ] = stopDaysimeters( handels )
 %STOPDAYSIMETERS Summary of this function goes here
 %   Detailed explanation goes here
+set(handels.daysimFound,'visible','off');
+daysims = daysimeter12.getDaysimeters();
+stop = daysimeter12.daysimeterStatus(0);
 
-
+saveloc = getenv('DAYSIMSAVELOC');
+for iDaysim = 1:length(daysims)
+    logInfoTxt = daysimeter12.readloginfo(fullfile(daysims{iDaysim},'log_info.txt'));
+    device = daysimeter12.parseDeviceSn(logInfoTxt);
+    message = daysimeter12.setStatus(fullfile(daysims{iDaysim},'log_info.txt'), stop);
+    if isempty(message)
+    else
+        set(handels.error,'visible','on');
+        set(handels.errorControl.instructBlock,'String',['There was an error writing the Status to daysimeter in daysimeter ',  device]);
+    end
+    newFileName = daysimeter12.makeNameStub(device);
+    copyfile(fullfile(daysims{iDaysim},'log_info.txt'),fullfile(saveloc,[newFileName,'-LOG.txt']));
+    copyfile(fullfile(daysims{iDaysim},'data_log.txt'),fullfile(saveloc,[newFileName,'-DATA.txt']));
+end
+set(handels.daysimSearch,'visible','on');
+set(handels.search.instructBlock,'string',sprintf('Daysimeters Stopped and Downloaded. \nPlease Select what you would like to do.'));
 end
 
