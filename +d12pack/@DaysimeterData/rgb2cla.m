7d3b43ae110223f927590330c8e2d22164776e2b
Geoffrey Jones <jonesg5@rpi.edu>
2016-04-26 13:15:20 -0400

    Cleaned up rgb2cla

--- +d12pack/@DaysimeterData/rgb2cla.m
+++ +d12pack/@DaysimeterData/rgb2cla.m
@@ -1,43 +1,28 @@
 function CircadianLight = rgb2cla(Red,Green,Blue)
-%RGB2CLA Summary of this function goes here
-%   Detailed explanation goes here
+%RGB2CLA Convert Daysimeter Red, Green, and Blue channels (RGB) to
+% Circadian Light (CLA)
 
-C = [0.617848 3.221534 0.265128 2.309656];
-
-M = [0.000254 0.167237 0.261462];
-
+% Define constants
+C  = [ 0.617848  3.221534 0.265128 2.309656];
+M  = [ 0.000254  0.167237 0.261462];
 Sm = [-0.005701 -0.014015 0.241859];
+Vm = [ 0.381876  0.642883 0.067544];
+Vp = [ 0.004458  0.360213 0.189536];
 
-Vm = [0.381876 0.642883 0.067544];
+RGB = horzcat(Red(:),Green(:),Blue(:));
 
-Vp = [0.004458 0.360213 0.189536];
+Scone      = sum(bsxfun(@times, Sm, RGB), 2);
+Vmaclamda  = sum(bsxfun(@times, Vm, RGB), 2);
+Melanopsin = sum(bsxfun(@times, M,  RGB), 2);
+Vprime     = sum(bsxfun(@times, Vp, RGB), 2);
 
-% CircadianLight = zeros(numel(Red),1); % Preallocate CircadianLight
-% for i1 = 1:length(Red)
-%     RGB = [Red(i1) Green(i1) Blue(i1)];
-%     Scone(i1,1) = sum(Sm.*RGB);
-%     Vmaclamda(i1,1) = sum(Vm.*RGB);
-%     Melanopsin(i1,1) = sum(M.*RGB);
-%     Vprime(i1,1) = sum(Vp.*RGB);
-%     
-%     if(Scone(i1,1) > C(3)*Vmaclamda(i1,1))
-%         CircadianLight(i1,1) = Melanopsin(i1,1) + C(1)*(Scone(i1,1) - C(3)*Vmaclamda(i1,1)) - C(2)*683*(1 - 2.71^(-(Vprime(i1,1)/(683*6.5))));
-%     else
-%         CircadianLight(i1,1) = Melanopsin(i1,1);
-%     end
-%     
-%     CircadianLight(i1,1) = C(4)*CircadianLight(i1,1);
-% end
+Temp = Melanopsin;
+idx  = Scone > C(3)*Vmaclamda;
+Temp(idx) = Melanopsin(idx) ...
+    + C(1)*(Scone(idx) - C(3)*Vmaclamda(idx)) ...
+    - C(2)*683*(1 - 2.71.^(-(Vprime(idx)/(683*6.5))));
 
-RGB2 = horzcat(Red(:),Green(:),Blue(:));
-Scone2 = sum(bsxfun(@times,Sm,RGB2),2);
-Vmaclamda2 = sum(bsxfun(@times,Vm,RGB2),2);
-Melanopsin2 = sum(bsxfun(@times,M,RGB2),2);
-Vprime2 = sum(bsxfun(@times,Vp,RGB2),2);
-intermediateCircadianLight2 = Melanopsin2;
-idx = Scone2 > C(3)*Vmaclamda2;
-intermediateCircadianLight2(idx) = Melanopsin2(idx) + C(1).*(Scone2(idx) - C(3).*Vmaclamda2(idx)) - C(2).*683.*(1 - 2.71.^(-(Vprime2(idx)./(683.*6.5))));
-CircadianLight = C(4)*intermediateCircadianLight2;
+CircadianLight = C(4)*Temp;
 
 end
 
