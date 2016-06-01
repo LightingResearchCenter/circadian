45af10f760b6b97bebe99c3f8818565b52ec766e
Geoffrey Jones <jonesg5@rpi.edu>
2016-06-01 11:57:40 -0400

    Enabled work mask, started button callbacks

--- +d12pack/maskingUI.m
+++ +d12pack/maskingUI.m
@@ -9,9 +9,11 @@ classdef maskingUI < matlab.mixin.SetGet
         ObservationRadioButton      matlab.ui.control.UIControl     % Observation
         ErrorRadioButton            matlab.ui.control.UIControl     % Error
         NoncomplianceRadioButton	matlab.ui.control.UIControl     % Noncompliance
-        LabelSwitch                 matlab.ui.control.UIControl     % Show in Be...
-        ShowBedSwitch               matlab.ui.control.UIControl     % On, Off
-        LabelLamp                   matlab.ui.control.UIControl     % Changes Saved
+        ShowBedLabel                matlab.ui.control.UIControl     % Show in Be...
+        ShowBedSwitch               matlab.ui.control.UIControl     % 0, 1
+        ShowWorkLabel               matlab.ui.control.UIControl     % Show in Wo...
+        ShowWorkSwitch              matlab.ui.control.UIControl     % 0, 1
+        ChangesSavedLabel           matlab.ui.control.UIControl     % Changes Saved
         ChangesSavedLamp            d12pack.lamp
         StartButton                 matlab.ui.control.UIControl     % Select Start
         EndButton                   matlab.ui.control.UIControl     % Select End
@@ -28,10 +30,21 @@ classdef maskingUI < matlab.mixin.SetGet
         Data % Description
     end
     
-    properties (Access = private)
+    properties (Access = private, SetObservable)
         hCS
         hAI
         hBed % Description
+        hWork
+        TempData
+    end
+    
+    properties (Access = private)
+        Blue   = [180, 211, 227]/255; % circadian stimulus
+        Grey   = [226, 226, 226]/255; % excluded data
+        Red    = [255, 215, 215]/255; % error
+        Yellow = [255, 255, 191]/255; % noncompliance
+        Purple = [186, 141, 186]/255; % in bed
+        Green  = [124, 197, 118]/255; % at work
     end
     
     %% Public Methods
@@ -69,6 +82,17 @@ classdef maskingUI < matlab.mixin.SetGet
             app.plotData;
         end
         
+        
+        % Save changes
+        function app = saveChanges(app)
+            app.Data = app.TempData;
+        end
+        
+        %Discard changes
+        function app = discardCahnges(app)
+            app.TempData = app.Data;
+        end
+        
     end
     
     %% Private Methods
@@ -76,31 +100,45 @@ classdef maskingUI < matlab.mixin.SetGet
         
         % Code that executes after component creation
         function app = startupFcn(app)
-            addlistener(app,'Data','PostSet',@app.DataChanged);
+            addlistener(app,'Data','PostSet',@app.DataSavedCallback);
+            addlistener(app,'TempData','PostSet',@app.DataChangedCallback);
         end
         
         function app = plotData(app)
             hold(app.DataAxes,'on');
             
+            t0 = datenum(app.Data.Time);
+            t1 = [t0(1);t0;t0(end)];
+            
             app.hBed = patch;
             app.hBed.Parent = app.DataAxes;
-            app.hBed.XData = datenum([app.Data.Time(1);app.Data.Time;app.Data.Time(end)]);
+            app.hBed.XData = t1;
             app.hBed.YData = [0;double(app.Data.InBed);0];
             app.hBed.EdgeColor = 'none';
-            app.hBed.FaceColor = [186, 141, 186]/255;
+            app.hBed.FaceColor = app.Purple;
             app.hBed.DisplayName = 'In Bed';
+            
+            app.hWork = patch;
+            app.hWork.Parent = app.DataAxes;
+            app.hWork.XData = t1;
+            app.hWork.YData = [0;double(app.Data.AtWork);0];
+            app.hWork.EdgeColor = 'none';
+            app.hWork.FaceColor = app.Green;
+            app.hWork.DisplayName = 'At Work';
             
             app.hCS = patch;
             app.hCS.Parent = app.DataAxes;
-            app.hCS.XData = datenum([app.Data.Time(1);app.Data.Time;app.Data.Time(end)]);
+            app.hCS.XData = t1;
             app.hCS.YData = [0;app.Data.CircadianStimulus;0];
             app.hCS.DisplayName = 'Circadian Stimulus (CS)';
             app.hCS.EdgeColor = 'none';
-            app.hCS.FaceColor = [180, 211, 227]/255;
+            app.hCS.FaceColor = app.Blue;
             
-            app.hAI = plot(app.DataAxes,app.Data.Time,app.Data.ActivityIndex);
+            app.hAI = plot(app.DataAxes,...
+                app.Data.Time,app.Data.ActivityIndex,...
+                'Color','black',...
+                'LineWidth',0.5);
             app.hAI.DisplayName = 'Activity Index';
-            app.hAI.Color = 'black';
             
             hold(app.DataAxes,'off');
             
@@ -108,14 +146,20 @@ classdef maskingUI < matlab.mixin.SetGet
         end
         
         function app = refreshdata(app)
-            app.hCS.XData = datenum([app.Data.Time(1);app.Data.Time;app.Data.Time(end)]);
+            t0 = datenum(app.Data.Time);
+            t1 = [t0(1);t0;t0(end)];
+            
+            app.hCS.XData = t1;
             app.hCS.YData = [0;app.Data.CircadianStimulus;0];
             
-            app.hAI.XData = datenum(app.Data.Time);
+            app.hAI.XData = t0;
             app.hAI.YData = app.Data.ActivityIndex;
             
-            app.hBed.XData = datenum([app.Data.Time(1);app.Data.Time;app.Data.Time(end)]);
+            app.hBed.XData = t1;
             app.hBed.YData = [0;double(app.Data.InBed);0];
+            
+            app.hWork.XData = t1;
+            app.hWork.YData = [0;double(app.Data.AtWork);0];
         end
         
     end
@@ -214,12 +258,12 @@ classdef maskingUI < matlab.mixin.SetGet
             app.NoncomplianceRadioButton.String = 'Noncompliance';
             app.NoncomplianceRadioButton.Position = [10 10 105 16];
             
-            % Create LabelSwitch
-            app.LabelSwitch = uicontrol(app.Figure);
-            app.LabelSwitch.Style = 'text';
-            app.LabelSwitch.HorizontalAlignment = 'center';
-            app.LabelSwitch.Position = [28 440 101 15];
-            app.LabelSwitch.String = 'Show in Bed Mask';
+            % Create ShowBedLabel
+            app.ShowBedLabel = uicontrol(app.Figure);
+            app.ShowBedLabel.Style = 'text';
+            app.ShowBedLabel.HorizontalAlignment = 'center';
+            app.ShowBedLabel.Position = [28 440 101 15];
+            app.ShowBedLabel.String = 'Show in Bed Mask';
             
             % Create ShowBedSwitch
             app.ShowBedSwitch = uicontrol(app.Figure);
@@ -228,12 +272,26 @@ classdef maskingUI < matlab.mixin.SetGet
             app.ShowBedSwitch.Position = [160 437 45 20];
             app.ShowBedSwitch.Value = app.ShowBedSwitch.Max;
             
+            % Create ShowWorkLabel
+            app.ShowWorkLabel = uicontrol(app.Figure);
+            app.ShowWorkLabel.Style = 'text';
+            app.ShowWorkLabel.HorizontalAlignment = 'center';
+            app.ShowWorkLabel.Position = [28 410 101 15];
+            app.ShowWorkLabel.String = 'Show at Work Mask';
+            
+            % Create ShowWorkSwitch
+            app.ShowWorkSwitch = uicontrol(app.Figure);
+            app.ShowWorkSwitch.Style = 'checkbox';
+            app.ShowWorkSwitch.Callback = @app.ShowWorkSwitchValueChanged;
+            app.ShowWorkSwitch.Position = [160 407 45 20];
+            app.ShowWorkSwitch.Value = app.ShowBedSwitch.Max;
+            
             % Create LabelLamp
-            app.LabelLamp = uicontrol(app.Figure);
-            app.LabelLamp.Style = 'text';
-            app.LabelLamp.HorizontalAlignment = 'right';
-            app.LabelLamp.Position = [28 480 88 15];
-            app.LabelLamp.String = 'Changes Saved';
+            app.ChangesSavedLabel = uicontrol(app.Figure);
+            app.ChangesSavedLabel.Style = 'text';
+            app.ChangesSavedLabel.HorizontalAlignment = 'right';
+            app.ChangesSavedLabel.Position = [28 480 88 15];
+            app.ChangesSavedLabel.String = 'Changes Saved';
             
             % Create ChangesSavedLamp
             app.ChangesSavedLamp = d12pack.lamp(app.Figure);
@@ -244,30 +302,38 @@ classdef maskingUI < matlab.mixin.SetGet
             app.StartButton.Style = 'pushbutton';
             app.StartButton.Position = [61 272 100 22];
             app.StartButton.String = 'Select Start';
+            app.StartButton.Callback = @app.StartButtonCallback;
             
             % Create EndButton
             app.EndButton = uicontrol(app.Figure);
             app.EndButton.Style = 'pushbutton';
             app.EndButton.Position = [61 230 100 22];
             app.EndButton.String = 'Select End';
+            app.EndButton.Callback = @app.EndButtonCallback;
+            app.EndButton.Enable = 'off';
             
             % Create SaveButton
             app.SaveButton = uicontrol(app.Figure);
             app.SaveButton.Style = 'pushbutton';
             app.SaveButton.Position = [61 188 100 22];
             app.SaveButton.String = 'Save Changes';
+            app.SaveButton.Callback = @app.SaveButtonCallback;
+            app.SaveButton.Enable = 'off';
             
             % Create CancelButton
             app.CancelButton = uicontrol(app.Figure);
             app.CancelButton.Style = 'pushbutton';
             app.CancelButton.Position = [61 146 100 22];
             app.CancelButton.String = 'Cancel Changes';
+            app.CancelButton.Callback = @app.CancelButtonCallback;
+            app.CancelButton.Enable = 'off';
             
             % Create ExitButton
             app.ExitButton = uicontrol(app.Figure);
             app.ExitButton.Style = 'pushbutton';
             app.ExitButton.Position = [61 41 100 22];
             app.ExitButton.String = 'Save & Exit';
+            app.ExitButton.Callback = @app.ExitButtonCallback;
         end
     end
     
@@ -278,8 +344,8 @@ classdef maskingUI < matlab.mixin.SetGet
             value = app.ShowBedSwitch.Value;
             switch value
                 case app.ShowBedSwitch.Max % On state
-                    app.refreshdata;
                     app.hBed.Visible = 'on';
+                    app.refreshdata;
                 case app.ShowBedSwitch.Min % Off state
                     app.hBed.Visible = 'off';
                 otherwise
@@ -287,9 +353,59 @@ classdef maskingUI < matlab.mixin.SetGet
             end
         end
         
-        % Data value changed function
-        function DataChanged(app,src,evnt)
+        % ShowWorkSwitch value changed function
+        function ShowWorkSwitchValueChanged(app,src,callbackData)
+            value = app.ShowWorkSwitch.Value;
+            switch value
+                case app.ShowWorkSwitch.Max % On state
+                    app.hWork.Visible = 'on';
+                    app.refreshdata;
+                case app.ShowWorkSwitch.Min % Off state
+                    app.hWork.Visible = 'off';
+                otherwise
+                    error('ShowBedSwitch value not recognized');
+            end
+        end
+        
+        % Data saved callback
+        function DataSavedCallback(app,src,evnt)
+            app.ChangesSavedLamp.Value = 'on';
+        end
+        
+        % TempData modified callback
+        function DataChangedCallback(app,src,evnt)
             app.ChangesSavedLamp.Value = 'off';
+        end
+        
+        function StartButtonCallback(app,src,callbackData)
+            app.StartButton.Enable = 'off';
+            app.EndButton.Enable = 'on';
+        end
+        
+        function EndButtonCallback(app,src,callbackData)
+            
+            app.EndButton.Enable = 'off';
+            app.SaveButton.Enable = 'on';
+            app.CancelButton.Enable = 'on';
+        end
+        
+        function SaveButtonCallback(app,src,callbackData)
+            
+            app.SaveButton.Enable = 'off';
+            app.CancelButton.Enable = 'off';
+            app.StartButton.Enable = 'on';
+        end
+        
+        function CancelButtonCallback(app,src,callbackData)
+            
+            app.SaveButton.Enable = 'off';
+            app.CancelButton.Enable = 'off';
+            app.StartButton.Enable = 'on';
+        end
+        
+        function ExitButtonCallback(app,src,callbackData)
+            app.saveChanges;
+            app.delete;
         end
     end
 end
