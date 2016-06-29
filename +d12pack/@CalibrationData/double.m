bd942fc1c0f53d10002331c434b6d291936fed14
Geoffrey Jones <jonesg5@rpi.edu>
2016-06-29 14:31:05 -0400

    D12pack dev (#10)
    * Changed rgb2cla to use exp instead of 2.71
    
    * Added input validation to BedLogData import
    
    * Added function to convert to HumanData from txt, cdf, and logs
    
    * Fixed typo
    
    * Added WorkLogData class
    
    * Added WorkLog and AtWork to HumanData
    
    * Modified WorkLogData to accommodate fixed schedules
    
    * Turned off table warnings changed error to warning for import methods
    
    * Created isAtWork method and isFedHoliday
    
    * Implemented WorkLog import and isAtWork
    
    * Created LocationData class
    
    * Created StaticData class
    
    * Changed Phasor to use PhasorData class
    
    * Fixed error trying to calibrate when no calibration data or source data exists
    
    * Fixed applyCalibration
    
    * Created and implemented determineRatio
    
    * Added analysis view for HumanData
    
    * Added alternate methods for determineRatio
    
    Switch uses RatioMethod property
    
    * Created method hourly
    
    * Fixed determineRatio and added hourly means for lux, CLA, and CS
    
    * Added WindowProximity to LocationData
    
    * Added and implemented WeatherLogData
    
    * Added Session property
    
    * Bulk commit
    
    * Over due commit
    
    * Additional averages
    
    * Started new reports classes
    
    * Fleshed out daysigram
    
    * Enabled resize on orientation and paper type change
    
    * Created skeleton for composite report
    
    * Started mask and maskingUI
    
    * Enabled work mask, started button callbacks
    
    * Completed maskingUI
    
    * Added miller and isiv
    
    * Completed composite
    
    * Added Notes column to calibration log format
    
    * Reverted calibration format for import
    
    * Added fields to table
    
    * Added CorrectionFactor property
    
    * Added new CalibrationRatio options
    
    * Removed d12pack

--- +d12pack/@CalibrationData/double.m
+++
@@ -1,8 +0,0 @@
-function Coefficients = double(obj)
-%DOUBLE Overloads double converter method
-%   Returns array in format [R,G,B]
-
-Coefficients = horzcat(vertcat(obj.Red),vertcat(obj.Green),vertcat(obj.Blue));
-
-end
-
